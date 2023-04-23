116cfae5643ac5664f915a23ff952b385eb8bfd2
fparejam <fparejamayo@gmail.com>
2023-04-23 20:17:22 +0200

    changed relative path on local to absolute path on PythonAnywhere

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dataFile', 'data.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/fparejam/backend-flask/dataFile/data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
